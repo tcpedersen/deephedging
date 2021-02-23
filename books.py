fixed overflow issue in normalization

books.py
@@ -176,45 +176,6 @@ def black_delta(time_to_maturity, spot, strike, rate, volatility, theta):
 
     return _theta * norm_cdf(_theta * (m / v + v / 2.))
 
-# @tf.function
-# def simulate_geometric_brownian_motion(maturity: float,
-#                                        init_state: tf.Tensor,
-#                                        drift: tf.Tensor,
-#                                        volatility: tf.Tensor,
-#                                        correlation: tf.Tensor,
-#                                        num_paths: int,
-#                                        num_steps: int) -> tf.Tensor:
-#     """Simulate a multivariate GBM.
-#     Args:
-#         maturity: float
-#         init_state: (instrument_dim, )
-#         drift : (instrument_dim, )
-#         volatility: (instrument_dim, )
-#         correlation : (instrument_dim, instrument_dim)
-#         num_paths : int
-#         num_steps : int
-#     Returns:
-#         Sample paths: (num_paths, instrument_dim, num_steps + 1)
-#     """
-#     zero_mean = tf.zeros_like(drift)
-#     size = (num_paths, num_steps)
-#     scale_tril = tf.linalg.cholesky(correlation)
-#     normal = tfp.distributions.MultivariateNormalTriL
-#     rvs = normal(zero_mean, scale_tril).sample(size)
-
-#     dt = maturity / num_steps
-#     m = (drift - volatility * volatility / 2.) * dt
-#     v = volatility * math.sqrt(dt)
-#     rvs = tf.exp(m + v * rvs)
-
-#     #paths = [tf.tile(init_state[tf.newaxis, :], (num_paths, 1))]
-#     paths = [init_state]
-
-#     for idx in range(num_steps):
-#         paths.append(paths[-1] * rvs[:, idx])
-
-#     return paths
-
 
 class BlackScholesPutCallBook(DerivativeBook):
     def __init__(self,
@@ -418,6 +379,28 @@ class BlackScholesPutCallBook(DerivativeBook):
         return self.exposure[tf.newaxis, :, tf.newaxis] * gradient
 
 
+def random_black_scholes_parameters(
+        maturity: int,
+        instrument_dim: int,
+        num_brownian_motions: int,
+        seed: int):
+
+    maturity = float(maturity)
+    drift = tf.random.uniform((instrument_dim, ), 0.05, 0.1, dtype=FLOAT_DTYPE)
+    rate = 0. # tf.random.uniform((1, ), 0.0, 0.05, dtype=FLOAT_DTYPE) # TODO
+
+    scale = tf.sqrt(float(num_brownian_motions))
+    diffusion = tf.random.uniform(
+        (instrument_dim, num_brownian_motions),
+        0.1 / scale, 0.3 / scale, FLOAT_DTYPE)
+
+    init_risky = uniform((instrument_dim, ), 75, 125, FLOAT_DTYPE)
+    init_riskless = uniform((1, ), 0.75, 1.25, FLOAT_DTYPE)
+    init_state = tf.concat([init_risky, init_riskless], axis=0)
+
+    return maturity, init_state, drift, rate, diffusion
+
+
 def random_black_scholes_put_call_book(
         maturity: float,
         book_size: int,
@@ -426,26 +409,26 @@ def random_black_scholes_put_call_book(
         seed: int):
     tf.random.set_seed(seed)
 
-    maturity = float(maturity)
+    maturity, init_state, drift, rate, diffusion = \
+        random_black_scholes_parameters(
+            maturity, instrument_dim, num_brownian_motions, seed)
+
     strike = uniform((book_size, ), 75, 125, FLOAT_DTYPE)
-    drift = tf.random.uniform((instrument_dim, ), 0.05, 0.1, dtype=FLOAT_DTYPE)
-    rate = 0. # tf.random.uniform((1, ), 0.0, 0.05, dtype=FLOAT_DTYPE) # TODO
-    diffusion = tf.random.uniform(
-        (instrument_dim, num_brownian_motions), 0, 0.25 / num_brownian_motions, FLOAT_DTYPE)
-    put_call = tf.cast(2 * tfp.distributions.Binomial(1, probs=0.5).sample(book_size) - 1, FLOAT_DTYPE)
-    exposure = tf.cast(2 * tfp.distributions.Binomial(1, probs=0.5).sample(book_size) - 1, FLOAT_DTYPE)
+    put_call = tf.cast(2 * tfp.distributions.Binomial(1, probs=0.5).sample(
+        book_size) - 1, FLOAT_DTYPE)
+    exposure = tf.cast(2 * tfp.distributions.Binomial(1, probs=0.5).sample(
+        book_size) - 1, FLOAT_DTYPE)
 
     if instrument_dim  > 1:
-        linker = tf.random.uniform((book_size, ), 0, instrument_dim - 1, INT_DTYPE)
+        linker = tf.random.uniform((book_size, ), 0, instrument_dim - 1,
+                                   INT_DTYPE)
     else:
         linker = tf.convert_to_tensor((0, ), INT_DTYPE)
 
     book = BlackScholesPutCallBook(
         maturity, strike, drift, rate, diffusion, put_call, exposure, linker)
 
-    init_risky = uniform((instrument_dim, ), 75, 125, FLOAT_DTYPE)
-    init_riskless = uniform((1, ), 0.75, 1.25, FLOAT_DTYPE)
-    init_state = tf.concat([init_risky, init_riskless], axis=0)
+
 
     return init_state, book
 

experiments/experiment-2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, LearningRateScheduler
 
-from models import SimpleHedge, DeltaHedge, EntropicRisk, ExpectedShortfall, CostSimpleHedge, CostDeltaHedge
+import models
 from books import random_simple_put_call_book, random_black_scholes_put_call_book
 from constants import FLOAT_DTYPE
 from utils import PeakSchedule, MeanVarianceNormaliser
@@ -25,8 +25,8 @@ def train_model(model, inputs, alpha, normalise=True):
     norm_inputs = [norm_information, inputs[1], inputs[2]]
 
     # compile model
-    risk_measure = ExpectedShortfall(alpha)
-    optimizer = tf.keras.optimizers.Adam()
+    risk_measure = models.ExpectedShortfall(alpha)
+    optimizer = tf.keras.optimizers.Adam(1e-1)
     model.compile(risk_measure, optimizer=optimizer)
 
     # define callbacks
@@ -104,15 +104,19 @@ time, train_samples = book.sample_paths(
 # === train simple model
 train = split_sample(train_samples)
 
-simple_model = CostSimpleHedge(num_steps, book.instrument_dim, 2, 15, cost)
-_, _, normaliser = train_model(simple_model, train, alpha)
+simple_model = models.CostRecurrentHedge(
+    num_steps, book.instrument_dim, 2, 15, cost)
+history, norm_train, normaliser = train_model(simple_model, train, alpha)
 
 
 # ==============================================================================
 # === train benchmark
 benchmark = [train[1], train[1], train[2]]
 
-benchmark_model = CostDeltaHedge(num_steps, book, cost)
+benchmark_model = models.DeltaHedge(num_steps, book)
+if cost is not None:
+    benchmark_model.add_cost_layers(cost)
+
 _, _, _ = train_model(benchmark_model, benchmark, alpha, False)
 
 
@@ -120,13 +124,9 @@ _, _, _ = train_model(benchmark_model, benchmark, alpha, False)
 # === train no liability
 no_liability = [train[0], train[1], tf.zeros_like(train[2])]
 
-no_liability_model = CostSimpleHedge(num_steps, book.instrument_dim, 2, 15, cost)
+no_liability_model = models.CostRecurrentHedge(
+    num_steps, book.instrument_dim, 2, 15, cost)
 _, _, _ = train_model(no_liability_model, no_liability, alpha)
-
-
-# ==============================================================================
-# === delete train data
-del train_samples, train
 
 
 # ==============================================================================
@@ -181,3 +181,23 @@ print(f"no liability total risk: {no_liability_risk - no_liability_price:5f}")
 plot_distributions([simple_model, benchmark_model],
                    [norm_test, norm_benchmark],
                    [simple_model_price, benchmark_price])
+
+
+
+
+
+
+
+num_train_paths = int(10**6)
+num_steps = 7
+
+init_state, book = random_black_scholes_put_call_book(
+    num_steps / 250, 25, 10, 10, 69)
+time, train_samples = book.sample_paths(
+    init_state, num_train_paths, num_steps, False)
+
+x = tf.math.log(train_samples / train_samples[..., 0, tf.newaxis])
+
+mean, variance = tf.nn.moments(x, 0)
+xn = tf.nn.batch_normalization(x, mean, variance, None, None, FLOAT_DTYPE_EPS)
+tf.nn.moments(xn, 0)

models.py
@@ -95,15 +95,9 @@ class Hedge(tf.keras.models.Model, abc.ABC):
 
 
     @property
-    @abc.abstractmethod
-    def instrument_dim(self) -> int:
-        """Returns number of hedge instruments."""
-
-
-    @property
-    @abc.abstractmethod
     def num_steps(self) -> int:
         """Returns number of steps in hedge."""
+        return len(self.strategy_layers)
 
 
     @property
@@ -118,14 +112,12 @@ class Hedge(tf.keras.models.Model, abc.ABC):
         return self._cost_layers
 
 
-    @property
-    def use_gradients(self) -> bool:
-        """Returns bool indicating whether to use gradients in hedge."""
-        return self._use_gradients
+    def add_cost_layers(self, cost: float):
+        if self._cost_layers:
+            raise ValueError("cost layers already exists.")
 
-    @use_gradients.setter
-    def use_gradients(self, val):
-        self._use_gradients = bool(val)
+        for _ in range(self.num_steps):
+            self._cost_layers.append(ProportionalCost(float(cost)))
 
 
     def compile(self, risk_measure, **kwargs):
@@ -169,7 +161,7 @@ class Hedge(tf.keras.models.Model, abc.ABC):
         Returns:
             (batch_size, )
         """
-        if self.cost_layers is not None:
+        if self.cost_layers:
             delta = new_hedge - old_hedge
             return self.cost_layers[step]([delta, instruments[..., step]])
 
@@ -191,10 +183,7 @@ class Hedge(tf.keras.models.Model, abc.ABC):
         Returns:
             value: (batch_size, )
         """
-        if self.use_gradients:
-            information, instruments, payoff, gradients = inputs
-        else:
-            information, instruments, payoff = inputs
+        information, instruments, payoff = inputs
 
         wealth = -payoff
         hedge = tf.zeros_like(instruments[..., 0], FLOAT_DTYPE)
@@ -203,12 +192,7 @@ class Hedge(tf.keras.models.Model, abc.ABC):
             observation = self.observation(step, information, hedge)
             old_hedge = hedge
             hedge = strategy(observation, training)
-
-            if self.use_gradients:
-                hedge += gradients[..., step]
-
             costs = self.costs(step, hedge, old_hedge, instruments)
-
             increment = instruments[..., step + 1] - instruments[..., step]
             wealth += tf.reduce_sum(tf.multiply(hedge, increment), 1)
             wealth -= costs
@@ -216,28 +200,18 @@ class Hedge(tf.keras.models.Model, abc.ABC):
         return wealth
 
 
+# ==============================================================================
+# === DeltaHedge
 class DeltaHedge(Hedge):
     def __init__(self, num_steps, book, **kwargs):
         super().__init__(**kwargs)
         self.book = book
-        self._num_steps = int(num_steps)
-
-        self._strategy_layers = [DeltaStrategy(book)] * self.num_steps
+        self._strategy_layers = [DeltaStrategy(book)] * num_steps
 
 
     @property
     def strategy_layers(self) -> list:
         return self._strategy_layers
-
-
-    @property
-    def instrument_dim(self) -> int:
-        return self.book.instrument_dim
-
-
-    @property
-    def num_steps(self) -> int:
-        return self._num_steps
 
 
     @property
@@ -252,26 +226,17 @@ class DeltaHedge(Hedge):
         return [step * self.dt, information[..., step]]
 
 
-class CostDeltaHedge(DeltaHedge):
-    def __init__(self, num_steps, book, cost, **kwargs):
-        super().__init__(num_steps, book, **kwargs)
-
-        for _ in range(self.num_steps):
-            self._cost_layers.append(ProportionalCost(cost))
-
-
+# ==============================================================================
+# === SimpleHedge
 class SimpleHedge(Hedge):
     def __init__(
             self, num_steps, instrument_dim, num_layers, num_units, **kwargs):
         super().__init__(**kwargs)
 
-        self._num_steps = int(num_steps)
-        self._instrument_dim = int(instrument_dim)
-
         self._strategy_layers = []
-        for _ in range(self.num_steps):
+        for _ in range(num_steps):
             self._strategy_layers.append(DenseStrategy(
-                self.instrument_dim, num_layers, num_units))
+                instrument_dim, num_layers, num_units))
 
 
     @property
@@ -279,17 +244,6 @@ class SimpleHedge(Hedge):
         return self._strategy_layers
 
 
-    @property
-    def instrument_dim(self) -> int:
-        return self._instrument_dim
-
-
-    @property
-    def num_steps(self) -> int:
-        return self._num_steps
-
-
-    @tf.function
     def observation(self, step, information, hedge):
         return information[..., step]
 
@@ -299,15 +253,48 @@ class CostSimpleHedge(SimpleHedge):
                  **kwargs):
         super().__init__(num_steps, instrument_dim, num_layers, num_units,
                          **kwargs)
+        super().add_cost_layers(cost)
 
-        self._cost_layers = []
-        for _ in range(self.num_steps):
-            self._cost_layers.append(ProportionalCost(cost))
-
-
-    @tf.function
     def observation(self, step, information, hedge):
-        return tf.concat([information[..., step], hedge], 1)
+        non_cost = super().observation(step, information, hedge)
+        return tf.concat([non_cost, hedge], 1)
+
+
+# ==============================================================================
+# === RecurrentHedge
+class RecurrentHedge(Hedge):
+    def __init__(
+            self, num_steps, instrument_dim, num_layers, num_units, **kwargs):
+        super().__init__(**kwargs)
+
+        self._strategy_layers = [
+            DenseStrategy(
+                instrument_dim,
+                num_layers,
+                num_units)] * num_steps
+
+
+    @property
+    def strategy_layers(self) -> list:
+        return self._strategy_layers
+
+
+    def observation(self, step, information, hedge):
+        time = tf.ones_like(information[..., step], FLOAT_DTYPE) * step
+        return tf.concat([time, information[..., step]], 1)
+
+
+class CostRecurrentHedge(RecurrentHedge):
+    def __init__(self, num_steps, instrument_dim, num_layers, num_units, cost,
+                 **kwargs):
+        super().__init__(num_steps, instrument_dim, num_layers, num_units,
+                         **kwargs)
+        super().add_cost_layers(cost)
+
+    def observation(self, step, information, hedge):
+        non_cost = super().observation(step, information, hedge)
+        return tf.concat([non_cost, hedge], 1)
+
 
 # =============================================================================
 # ===

tests/test_utils.py
@@ -8,10 +8,11 @@ from utils import MeanVarianceNormaliser
 from constants import FLOAT_DTYPE
 
 class test_MeanVarianceNormaliser(TestCase):
-    def test_transform(self):
+    def test_degenerate_col(self):
         batch_size, information_dim, num_steps = 6, 7, 8
         degenerate_col = 4
-        x = tf.random.uniform((batch_size, information_dim, num_steps), dtype=FLOAT_DTYPE)
+        x = tf.random.uniform((batch_size, information_dim, num_steps),
+                              -2., 10, FLOAT_DTYPE)
 
         # ensure one row degenerate
         mask = tf.equal(x, x[..., degenerate_col, None])
@@ -25,6 +26,32 @@ class test_MeanVarianceNormaliser(TestCase):
         expected_var = tf.ones_like(x[0, ...])
         expected_var = tf.where(mask, 0., expected_var)
         assert_near(tf.math.reduce_variance(xn, 0), expected_var)
+
+        y = normaliser.inverse_transform(xn)
+        assert_near(y, x)
+
+
+    def test_low_variance(self):
+        batch, height, width = 2**20, 4, 3
+        collector_out = []
+
+        for i in range(height):
+            collector_in = []
+            for j in range(width):
+                min_ = tf.random.uniform((), 1000, 1000.1, FLOAT_DTYPE)
+                max_ = tf.random.uniform((), 1000.1, 1000.2, FLOAT_DTYPE)
+                vals = tf.random.uniform((batch, ), min_, max_, FLOAT_DTYPE)
+
+                collector_in.append(vals)
+            collector_out.append(collector_in)
+        raw = tf.convert_to_tensor(collector_out, FLOAT_DTYPE)
+        x = tf.transpose(raw, [2, 0, 1])
+
+        normaliser = MeanVarianceNormaliser()
+        xn = normaliser.fit_transform(x)
+
+        assert_near(tf.reduce_mean(xn, 0), tf.zeros_like(x[0, ...]), atol=1e-2)
+        assert_near(tf.math.reduce_variance(xn, 0), tf.ones_like(x[0, ...]), atol=1e-2)
 
         y = normaliser.inverse_transform(xn)
         assert_near(y, x)

utils.py
@@ -55,8 +55,13 @@ class MeanVarianceNormaliser:
         Args:
             x: (batch_size, information_dim, num_steps + 1)
         '''
-        self.mean = tf.reduce_mean(x, 0)
-        self.std = tf.math.reduce_std(x, 0)
+
+        # major numerical overflow when sum exceeds dtype max in reduce_mean,
+        # so convert to np.float64 before calculating moments.
+        self.mean, self.variance = tf.cast(
+            tf.nn.moments(tf.cast(x, np.float64), 0), FLOAT_DTYPE)
+
+        self.eps = FLOAT_DTYPE_EPS
 
     def transform(self, x):
         '''Normalize data.
@@ -65,8 +70,9 @@ class MeanVarianceNormaliser:
         Returns:
             y: same as input.
         '''
-        y = (x - self.mean) / self.std
-        return tf.where(tf.equal(self.std, 0)[tf.newaxis, ...], 0., y)
+        y = tf.nn.batch_normalization(
+            x, self.mean, self.variance, None, None, self.eps)
+        return tf.where(tf.equal(self.variance, 0), 0., y)
 
     def fit_transform(self, x):
         self.fit(x)
@@ -79,5 +85,5 @@ class MeanVarianceNormaliser:
         Returns:
             x: same as input.
         '''
-        x = self.std * y + self.mean
-        return tf.where(tf.equal(self.std, 0.), self.mean, x)
+        x = tf.sqrt(self.variance + self.eps) * y + self.mean
+        return tf.where(tf.equal(self.variance, 0.), self.mean, x)